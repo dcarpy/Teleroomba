eb5f772ffafb2204880e542d64da19f8c2b5622f
Dave C <carpy@q.com>
2020-01-01 20:44:12 -0600

    Initial check and add joypad - dsc

--- webRTC_Control/teleroomba_Android/www/js/gamepad.js
+++ webRTC_Control/teleroomba_Android/www/js/gamepad.js
@@ -1,81 +1,55 @@
-var headset = {
-	pitch : 128,
-	roll : 90,
-	sendInterval : null,
-	init: null,
+//------Gamepad Control---------//
+
+var gamepad = {};
+
+gamepad.data = {
+	lV : 0,
+	rV : 0
+}
+
+
+window.addEventListener("load",function(){gamepad.init()});
+
+gamepad.init = function(){
+	joypad.set({ axisMovementThreshold: 0.3 });
+    joypad.on('connect', e => updateInfo(e));
+    joypad.on('disconnect', e => resetInfo(e));
+    joypad.on('axis_move', e => {
+        console.log(e.detail);
+        return moveBall(e)
+    });
+}
+
+
+function resetInfo(e) {
+    console.log(e);
+    //heading.innerText = 'No gamepad connected!';
+    //message.innerText = 'Please connect a controller and press any key to start.';
 };
 
-headset.orienUpdate = function(e) {
-	            headsetOrien = deviceOrientation(e);
-	            
-	            var pitch = Math.round(headsetOrien.gamma);
-				var roll = Math.round(headsetOrien.alpha);
-	            
-	            //covert roll to range 0~360
-	            if(pitch < 0){
-		            if(roll < 180){
-			            roll += 180;
-		            }
-		            else{
-			            roll -= 180;
-		            }
-	            }
-	            
-	            //covert roll to range 0~90~180
-	            if(roll > 90 && roll < 270){
-		            return;
-	            }
-		        else{
-			        if( roll <= 90 ){ //0~90
-						roll = 90 - roll;
-		            }else if( roll >= 270 ){ //90~180
-			            roll = 450 - roll;
-		            }
-		        }
-		        
-		        
-		        //covert pitch to range 0~90~180
-		        
-		        if(pitch < 0){
-			        pitch += 180; 
-		        }
-			    
-			    pitch = Math.floor((pitch/1.8) + 80);
-			    
-			    //console.log(pitch);
-				
-				headset.pitch = pitch;
-				headset.roll = roll;
-				
-				animate.rollIndiKnob.style.backgroundPosition = Math.round(headset.roll/1.8) + "%";
-	            
-}
+function updateInfo(e) {
+    //const { gamepad } = e;
+	console.log(e);
+    //heading.innerText = 'Gamepad connected!';
+    //message.innerText = gamepad.id + '\n\n' + 'Use the left stick to move the ball.';
+};
 
-headset.init = function(enabled) {
-            if (enabled === false) {
-                window.removeEventListener("deviceorientation", headset.orienUpdate);
-                //stop interval
-                
-                clearInterval(headset.sendInterval);
-                console.log('headset gyro disabled');
-                
-            } else {
-                window.addEventListener("deviceorientation", headset.orienUpdate);
-                //start interval
-                                
-                headset.sendInterval = setInterval(function(){
-	                
-	                var frontCam = {
-						type:"FC",
-						r : headset.roll,
-						p : headset.pitch
-					}
-					
-					WebRTCDataMethold.sendData(frontCam);
-	                
-                }, 40);
-                
-                console.log('headset gyro enabled');
-            }
+function moveBall(e) {
+    const { stickMoved, directionOfMovement, axisMovementValue } = e.detail;
+    if (stickMoved === 'right_stick') {
+        switch (directionOfMovement) {
+            case 'left':
+				console.log(axisMovementValue);
+                break;
+            case 'right':
+				console.log(axisMovementValue);
+                break;
+            case 'top':
+				console.log(axisMovementValue);
+                break;
+            case 'bottom':
+				console.log(axisMovementValue);
+                break;
+        }
+    }
 }
-
