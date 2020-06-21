e71237a8b5f16b1c787ce89139da977bf2d2c9eb
dcarpy <carpy@q.com>
2020-06-21 09:29:20 -0500

    Format changes - dsc

--- webRTC_Control/teleroomba_Android/www/js/gamepad_model2.js
+++ webRTC_Control/teleroomba_Android/www/js/gamepad_model2.js
@@ -1,251 +1,327 @@
 var gp = {
 
-  data: {
-    lV: 0,
-    rV: 0
-  },
-  knob: null,
-  mapKnob: null,
-  size: 80,
-  trigger: 0
+    data: {
+        lV: 0,
+        rV: 0
+    },
+    knob: null,
+    mapKnob: null,
+    size: 80,
+    trigger: 0
 
 }
 
-var haveEvents = 'ongamepadconnected' in window
-var controllers = {}
-var gamepadLastTimestamp = -1
-var throttle = 255
-gp.throttle = (throttle / 255).toPrecision(3)
+var haveEvents = 'ongamepadconnected' in window;
+var controllers = {};
+var gamepadLastTimestamp = -1;
+var throttle = 255;
+gp.throttle = (throttle / 255).toPrecision(3);
 
 window.addEventListener('load', function () {
-  gp.init(80)
+    gp.init(80);
 })
 
 gp.init = function (size) {
-  gp.knob = document.getElementById('EX3DShow_knob')
-  gp.mapKnob = document.getElementById('EX3DShow_map')
-  var range = document.getElementById('EX3DShow_range')
+    gp.knob = document.getElementById('EX3DShow_knob');
+    gp.mapKnob = document.getElementById('EX3DShow_map');
+    var range = document.getElementById('EX3DShow_range');
 
-  if (size) {
-    gp.size = size
-  }
+    if (size) {
+        gp.size = size;
+    };
 
-  range.style.height = size * 2 + 'px'
-  range.style.width = size * 2 + 'px'
+    range.style.height = size * 2 + 'px';
+    range.style.width = size * 2 + 'px';
 }
 
 gp.calcuDrive = function (roll, pitch) {
-  var deltaX = roll
-  var deltaY = pitch
+    var deltaX = roll;
+    var deltaY = pitch;
 
-  gp.knob.style.transform = 'translateX(' + deltaX * gp.size + 'px) translateY(' + deltaY * gp.size + 'px)'
+    gp.knob.style.transform = 'translateX(' + deltaX * gp.size + 'px) translateY(' + deltaY * gp.size + 'px)';
 
-  var arcTanValue = Math.atan(deltaY / deltaX)
+    var arcTanValue = Math.atan(deltaY / deltaX);
 
-  // console.log( arcTanValue / Math.PI * 360 );
+    // console.log( arcTanValue / Math.PI * 360 );
 
-  var ratio = null
-  var map = {}
+    var ratio = null;
+    var map = {};
 
-  ratio = Math.sin(arcTanValue)
+    ratio = Math.sin(arcTanValue);
 
-  if (Math.abs(deltaY / deltaX) > 1) {
-    ratio = Math.sin(arcTanValue)
-    map = getMapVal()
+    if (Math.abs(deltaY / deltaX) > 1) {
+        ratio = Math.sin(arcTanValue);
+        map = getMapVal();
+
+        if (deltaY < 0) {
+            map.x = -map.x;
+            map.y = -map.y;
+        }
+    } else {
+        ratio = Math.cos(arcTanValue);
+        map = getMapVal();
+
+        if (deltaX < 0) {
+            map.x = -map.x;
+            map.y = -map.y;
+        }
+    }
+
+    function getMapVal() {
+        var map = {};
+        var disDelta = (Math.sqrt(Math.pow(deltaX * gp.size, 2) + Math.pow(deltaY * gp.size, 2)));
+        map.r = disDelta * ratio * gp.throttle;
+        map.x = Math.cos(arcTanValue) * map.r;
+        map.y = Math.sin(arcTanValue) * map.r;
+
+        return map;
+    }
+
+    gp.mapKnob.style.transform = 'translateX(' + map.x + 'px) translateY(' + map.y + 'px)';
+
+    var power = Math.abs((map.r / gp.size).toPrecision(3));
+    var dir = (arcTanValue / Math.PI * 180);
+    if (dir < 0) {
+        dir += 180;
+    }
+    dir = Number(dir.toPrecision(3));
 
     if (deltaY < 0) {
-      map.x = -map.x
-      map.y = -map.y
-    }
-  } else {
-    ratio = Math.cos(arcTanValue)
-    map = getMapVal()
+        map = getMapVal();
 
-    if (deltaX < 0) {
-      map.x = -map.x
-      map.y = -map.y
-    }
-  }
+        gp.data.lV = parseInt((dir / 45 - 1) * power * power * 50);
+        gp.data.rV = parseInt((3 - dir / 45) * power * power * 50);
 
-  function getMapVal () {
-    var map = {}
-    var disDelta = (Math.sqrt(Math.pow(deltaX * gp.size, 2) + Math.pow(deltaY * gp.size, 2)))
-    map.r = disDelta * ratio * gp.throttle
-    map.x = Math.cos(arcTanValue) * map.r
-    map.y = Math.sin(arcTanValue) * map.r
+        var VLimit = Math.round(50 * gp.throttle * gp.throttle);
 
-    return map
-  }
+        if (gp.data.lV > VLimit) {
+            gp.data.lV = VLimit;
+        }
+        if (gp.data.rV > VLimit) {
+            gp.data.rV = VLimit;
+        }
+    } else {
+        gp.data.lV = parseInt((dir / 45 - 1) * power * power * -50);
+        gp.data.rV = parseInt((3 - dir / 45) * power * power * -50);
 
-  gp.mapKnob.style.transform = 'translateX(' + map.x + 'px) translateY(' + map.y + 'px)'
+        var VLimit = Math.round(-50 * gp.throttle * gp.throttle);
 
-  var power = Math.abs((map.r / gp.size).toPrecision(3))
-  var dir = (arcTanValue / Math.PI * 180)
-  if (dir < 0) {
-    dir += 180
-  }
-  dir = Number(dir.toPrecision(3))
+        if (gp.data.lV < VLimit) {
+            gp.data.lV = VLimit;
+        }
 
-  if (deltaY < 0) {
-    map = getMapVal()
-
-    gp.data.lV = parseInt((dir / 45 - 1) * power * power * 50)
-    gp.data.rV = parseInt((3 - dir / 45) * power * power * 50)
-
-    var VLimit = Math.round(50 * gp.throttle * gp.throttle)
-
-    if (gp.data.lV > VLimit) {
-      gp.data.lV = VLimit
-    }
-    if (gp.data.rV > VLimit) {
-      gp.data.rV = VLimit
-    }
-  } else {
-    gp.data.lV = parseInt((dir / 45 - 1) * power * power * -50)
-    gp.data.rV = parseInt((3 - dir / 45) * power * power * -50)
-
-    var VLimit = Math.round(-50 * gp.throttle * gp.throttle)
-
-    if (gp.data.lV < VLimit) {
-      gp.data.lV = VLimit
+        if (gp.data.rV < VLimit) {
+            gp.data.rV = VLimit;
+        }
     }
 
-    if (gp.data.rV < VLimit) {
-      gp.data.rV = VLimit
+    if (isNaN(gp.data.rV) || isNaN(gp.data.lV)) {
+        gp.data = {
+            lV: 0,
+            rV: 0
+        }
     }
-  }
 
-  if (isNaN(gp.data.rV) || isNaN(gp.data.lV)) {
-    gp.data = {
-      lV: 0,
-      rV: 0
-    }
-  }
-
-  gp.data.type = 'DR'
-  console.log(gp.data)
-  WebRTCDataMethold.sendData(gp.data)
+    gp.data.type = 'DR';
+    console.log(gp.data);
+    WebRTCDataMethold.sendData(gp.data);
 }
 
-function connecthandler (e) {
-  console.log('Gamepad connected at index %d: %s. %d buttons, %d axes.',
-    e.gamepad.index, e.gamepad.id,
-    e.gamepad.buttons.length, e.gamepad.axes.length)
-  addgamepad(e.gamepad)
+gp.calcuDrive2 = function (roll, pitch) {
+
+    var deltaX = roll * 50;
+    var deltaY = pitch * 50;
+
+    var speedY = 0 - deltaY;
+    //console.log(speedY);
+
+    let speedVector = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    let arcTanValue = (Math.atan(deltaX / (0 - deltaY)) / (12 * Math.PI));
+    let arcCotValue = (Math.atan((0 - deltaY) / deltaX) / (12 * Math.PI));
+    let turningSpeed = Math.round(50 * (speedVector / 50) * 1.1);
+    let turningSpeedBack = Math.round(50 * (speedVector / 50) * 1.25);
+
+    if (turningSpeed >= 50) {
+        turningSpeed = 50;
+    }
+    if (turningSpeedBack >= 50) {
+        turningSpeedBack = 50;
+    }
+
+    //console.log(deltaX);
+
+    // forward
+    if (0 - deltaY >= 0) {
+        if (deltaX <= 0) {
+            console.log('turning left');
+            gp.data.lV = Math.round((speedVector * arcTanValue + (0 - deltaY)) * 1.1);
+            if (gp.data.lV >= 50) {
+                gp.data.lV = 50;
+            }
+            gp.data.rV = turningSpeed;
+        } else if (deltaX > 0) {
+            console.log('turning right');
+            gp.data.lV = turningSpeed;
+            gp.data.rV = Math.round(-1 * (speedVector * arcTanValue) + (0 - deltaY) * 1.1);
+            if (gp.data.rV >= 50) {
+                gp.data.rV = 50;
+            };
+        }
+        // backward
+    } else if (0 - deltaY <= 0) {
+        if (deltaX >= 40) {
+            console.log('turning right');
+            gp.data.lV = turningSpeedBack;
+            gp.data.rV = Math.round((speedVector * arcTanValue + (0 - deltaY) * 1.25));
+            if (gp.data.rV <= -50) {
+                gp.data.rV = -50;
+            }
+        } else if (deltaX < -40) {
+            console.log('turning left');
+            gp.data.lV = Math.round(-1 * (speedVector * arcTanValue) + (0 - deltaY) * 1.25);
+            if (gp.data.lV <= -50) {
+                gp.data.lV = -50;
+            };
+            gp.data.rV = turningSpeedBack;
+        } else if (deltaX > -25 && deltaX < 25) {
+            gp.data.lV = -10;
+            gp.data.rV = -10;
+        } else {
+            gp.data.lV = 0;
+            gp.data.rV = 0;
+        }
+    }
+
+    gp.data.type = 'DR';
+    console.log(gp.data);
+    WebRTCDataMethold.sendData(gp.data);
 }
 
-function addgamepad (gamepad) {
-  controllers[gamepad.index] = gamepad
-  requestAnimationFrame(updateStatus)
+function connecthandler(e) {
+    console.log('Gamepad connected at index %d: %s. %d buttons, %d axes.',
+        e.gamepad.index, e.gamepad.id,
+        e.gamepad.buttons.length, e.gamepad.axes.length);
+    addgamepad(e.gamepad);
 }
 
-function disconnecthandler (e) {
-  removegamepad(e.gamepad)
+function addgamepad(gamepad) {
+    controllers[gamepad.index] = gamepad;
+    requestAnimationFrame(updateStatus);
 }
 
-function removegamepad (gamepad) {
-  delete controllers[gamepad.index]
+function disconnecthandler(e) {
+    removegamepad(e.gamepad);
+}
+
+function removegamepad(gamepad) {
+    delete controllers[gamepad.index];
 }
 
 function buttonPressed(b) {
-  if (typeof(b) == "object") {
-    return b.pressed;
-  }
-  return b == 1.0;
-}
-
-function updateStatus () {
-  if (!haveEvents) {
-    scangamepads()
-  }
-
-  var i = 0
-
-  // use first gamepad detected
-  var controller = controllers[0]
-
-  var timestamp = controller.timestamp
-
-  if (timestamp !== gamepadLastTimestamp) {
-	
-	if (buttonPressed(controller.buttons[8])) {
-	  console.log('Button Back pressed')
-	}
-	
-	if (buttonPressed(controller.buttons[9])) {
-	  console.log('Button Start pressed')
-	}
-
-    const coord = { x: controller.axes[2], y: controller.axes[3] }
-    const deadzone = 0.10
-    const force = radial(coord, deadzone, normalise)
-    // console.log(force.x, force.y);
-
-    const x = controller.axes[2]
-    // console.log(x)
-    const y = controller.axes[3]
-    // console.log(y)
-
-    // gp.calcuDrive(x, y)
-    gp.calcuDrive(force.x, force.y)
-  }
-
-  gamepadLastTimestamp = timestamp
-
-  requestAnimationFrame(updateStatus)
-}
-
-function scangamepads () {
-  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : [])
-  for (var i = 0; i < gamepads.length; i++) {
-    if (gamepads[i]) {
-      if (gamepads[i].index in controllers) {
-        controllers[gamepads[i].index] = gamepads[i]
-      } else {
-        if (gamepads[i].mapping === 'standard') {
-          addgamepad(gamepads[i])
-        }
-      }
+    if (typeof(b) == "object") {
+        return b.pressed;
     }
-  }
+    return b == 1.0;
 }
 
-function raw (scalar) {
-  return scalar
+function updateStatus() {
+    if (!haveEvents) {
+        scangamepads();
+    }
+
+    var i = 0;
+
+    // use first gamepad detected
+    var controller = controllers[0];
+
+    var timestamp = controller.timestamp;
+
+    if (timestamp !== gamepadLastTimestamp) {
+
+        if (buttonPressed(controller.buttons[8])) {
+            console.log('Button Back pressed');
+        }
+
+        if (buttonPressed(controller.buttons[9])) {
+            console.log('Button Start pressed');
+        }
+
+        const coord = {
+            x: controller.axes[2],
+            y: controller.axes[3]
+        }
+        const deadzone = 0.10;
+        const force = radial(coord, deadzone, normalise);
+        // console.log(force.x, force.y);
+
+        const x = controller.axes[2];
+        // console.log(x);
+        const y = controller.axes[3];
+        // console.log(y);
+
+        // gp.calcuDrive(x, y);
+        gp.calcuDrive2(force.x, force.y);
+    }
+
+    gamepadLastTimestamp = timestamp;
+
+    requestAnimationFrame(updateStatus);
 }
 
-function normalise (scalar, deadzone = 0) {
-  if (scalar === 0) {
-    return scalar
-  }
-
-  const absScalar = Math.abs(scalar)
-  const normalised = (absScalar - deadzone) / (1 - deadzone)
-
-  return scalar < 0 ? -normalised : normalised
+function scangamepads() {
+    var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+    for (var i = 0; i < gamepads.length; i++) {
+        if (gamepads[i]) {
+            if (gamepads[i].index in controllers) {
+                controllers[gamepads[i].index] = gamepads[i];
+            } else {
+                if (gamepads[i].mapping === 'standard') {
+                    addgamepad(gamepads[i]);
+                }
+            }
+        }
+    }
 }
 
-function radial (coord, deadzone = 0, post = normalise) {
-  const angle = Math.atan2(coord.y, coord.x)
-  let magnitude = Math.sqrt(coord.x * coord.x + coord.y * coord.y)
-
-  if (magnitude <= deadzone) {
-    return { x: 0, y: 0 }
-  }
-
-  if (magnitude > 1) {
-    magnitude = 1
-  }
-
-  return {
-    x: Math.cos(angle) * post(magnitude, deadzone),
-    y: Math.sin(angle) * post(magnitude, deadzone)
-  }
+function raw(scalar) {
+    return scalar;
 }
 
-window.addEventListener('gamepadconnected', connecthandler)
-window.addEventListener('gamepaddisconnected', disconnecthandler)
+function normalise(scalar, deadzone = 0) {
+    if (scalar === 0) {
+        return scalar;
+    }
+
+    const absScalar = Math.abs(scalar);
+    const normalised = (absScalar - deadzone) / (1 - deadzone);
+
+    return scalar < 0 ? -normalised : normalised;
+}
+
+function radial(coord, deadzone = 0, post = normalise) {
+    const angle = Math.atan2(coord.y, coord.x);
+    let magnitude = Math.sqrt(coord.x * coord.x + coord.y * coord.y);
+
+    if (magnitude <= deadzone) {
+        return {
+            x: 0,
+            y: 0
+        }
+    }
+
+    if (magnitude > 1) {
+        magnitude = 1;
+    }
+
+    return {
+        x: Math.cos(angle) * post(magnitude, deadzone),
+        y: Math.sin(angle) * post(magnitude, deadzone);
+    }
+}
+
+window.addEventListener('gamepadconnected', connecthandler);
+window.addEventListener('gamepaddisconnected', disconnecthandler);
 
 if (!haveEvents) {
-  setInterval(scangamepads, 500)
+    setInterval(scangamepads, 500);
 }
--- webRTC_Control/teleroomba_Android/www/js/joystick_model2.js
+++ webRTC_Control/teleroomba_Android/www/js/joystick_model2.js
@@ -2,96 +2,116 @@
 
 console.log('touchscreen is', VirtualJoystick.touchScreenAvailable() ? 'available' : 'not available')
 
-var joystick = {}
+var joystick = {};
 
 joystick.data = {
-  lV: 0,
-  rV: 0
+    lV: 0,
+    rV: 0
 }
 
-//
 joystick.stick = new VirtualJoystick({
-  container: document.getElementById('touchLayer'),
-  func: 'drive',
-  strokeStyle: 'rgba(255,255,255,0.35)',
-  mouseSupport: true,
-  limitStickTravel: true,
-  stickRadius: 50
+    container: document.getElementById('touchLayer'),
+    func: 'drive',
+    strokeStyle: 'rgba(255,255,255,0.35)',
+    mouseSupport: true,
+    limitStickTravel: true,
+    stickRadius: 50
 })
 
 joystick.stick.addEventListener('mouseMove', function (event) {
-  if (joystick.stick.deltaX() + joystick.stick.deltaY() !== 0) {
-    joystick.calcuDrive()
-  }
+    if (joystick.stick.deltaX() + joystick.stick.deltaY() !== 0) {
+        joystick.calcuDrive()
+    }
 })
+
 joystick.stick.addEventListener('touchMove', function (event) {
-  if (joystick.stick.deltaX() + joystick.stick.deltaY() !== 0) {
-    joystick.calcuDrive()
-  }
+    if (joystick.stick.deltaX() + joystick.stick.deltaY() !== 0) {
+        joystick.calcuDrive()
+    }
 })
+
 joystick.stick.addEventListener('mouseUp', function (event) {
-  joystick.sendBrake()
+    joystick.sendBrake()
 })
+
 joystick.stick.addEventListener('touchEnd', function (event) {
-  joystick.sendBrake()
+    joystick.sendBrake()
 })
 
 joystick.calcuDrive = function () {
-  // var speedY = 0-joystick.stick.deltaY();
-  let speedVector = Math.sqrt(Math.pow(joystick.stick.deltaX(), 2) +
-                        Math.pow(joystick.stick.deltaY(), 2))
-  let arcTanValue = (Math.atan(joystick.stick.deltaX() / (0 - joystick.stick.deltaY())) / (12 * Math.PI))
-  let arcCotValue = (Math.atan((0 - joystick.stick.deltaY()) / joystick.stick.deltaX()) / (12 * Math.PI))
-  let turningSpeed = Math.round(50 * (speedVector / 50) * 1.1)
-  let turningSpeedBack = Math.round(50 * (speedVector / 50) * 1.25)
-  if (turningSpeed >= 50) {
-    turningSpeed = 50
-  }
-  if (turningSpeedBack >= 50) {
-    turningSpeedBack = 50
-  }
-  // console.log(arcTanValue);
+    var speedY = 0 - joystick.stick.deltaY();
+    console.log(speedY);
 
-  if (0 - joystick.stick.deltaY() >= 0) {
-    if (joystick.stick.deltaX() <= 0) {
-      joystick.data.lV = Math.round((speedVector * arcTanValue + (0 - joystick.stick.deltaY())) * 1.1)
-      if (joystick.data.lV >= 50) { joystick.data.lV = 50 }
-      joystick.data.rV = turningSpeed
-    } else if (joystick.stick.deltaX() > 0) {
-      joystick.data.lV = turningSpeed
-      joystick.data.rV = Math.round(-1 * (speedVector * arcTanValue) + (0 - joystick.stick.deltaY()) * 1.1)
-      if (joystick.data.rV >= 50) { joystick.data.rV = 50 };
+    let speedVector = Math.sqrt(Math.pow(joystick.stick.deltaX(), 2) +
+            Math.pow(joystick.stick.deltaY(), 2));
+    let arcTanValue = (Math.atan(joystick.stick.deltaX() / (0 - joystick.stick.deltaY())) / (12 * Math.PI));
+    let arcCotValue = (Math.atan((0 - joystick.stick.deltaY()) / joystick.stick.deltaX()) / (12 * Math.PI));
+    let turningSpeed = Math.round(50 * (speedVector / 50) * 1.1);
+    let turningSpeedBack = Math.round(50 * (speedVector / 50) * 1.25);
+    if (turningSpeed >= 50) {
+        turningSpeed = 50
     }
-  } else if (0 - joystick.stick.deltaY() <= 0) {
-    if (joystick.stick.deltaX() >= 40) {
-      joystick.data.lV = turningSpeedBack
-      joystick.data.rV = Math.round((speedVector * arcTanValue + (0 - joystick.stick.deltaY()) * 1.25))
-      if (joystick.data.rV <= -50) { joystick.data.rV = -50 }
-    } else if (joystick.stick.deltaX() < -40) {
-      joystick.data.lV = Math.round(-1 * (speedVector * arcTanValue) + (0 - joystick.stick.deltaY()) * 1.25)
-      if (joystick.data.lV <= -50) { joystick.data.lV = -50 };
-      joystick.data.rV = turningSpeedBack
-    } else if (joystick.stick.deltaX() > -25 && joystick.stick.deltaX() < 25) {
-      joystick.data.lV = -10
-      joystick.data.rV = -10
-    } else {
-      joystick.data.lV = 0
-      joystick.data.rV = 0
+    if (turningSpeedBack >= 50) {
+        turningSpeedBack = 50
     }
-  }
+    // console.log(arcTanValue);
 
-  joystick.data.type = 'DR'
-  WebRTCDataMethold.sendData(joystick.data)
+    console.log(joystick.stick.deltaX());
+
+    // forward
+    if (0 - joystick.stick.deltaY() >= 0) {
+        if (joystick.stick.deltaX() <= 0) {
+            console.log('turning left');
+            joystick.data.lV = Math.round((speedVector * arcTanValue + (0 - joystick.stick.deltaY())) * 1.1);
+            if (joystick.data.lV >= 50) {
+                joystick.data.lV = 50
+            }
+            joystick.data.rV = turningSpeed;
+        } else if (joystick.stick.deltaX() > 0) {
+            console.log('turning right');
+            joystick.data.lV = turningSpeed;
+            joystick.data.rV = Math.round(-1 * (speedVector * arcTanValue) + (0 - joystick.stick.deltaY()) * 1.1);
+            if (joystick.data.rV >= 50) {
+                joystick.data.rV = 50
+            };
+        }
+        // backward
+    } else if (0 - joystick.stick.deltaY() <= 0) {
+        if (joystick.stick.deltaX() >= 40) {
+            console.log('turning right');
+            joystick.data.lV = turningSpeedBack;
+            joystick.data.rV = Math.round((speedVector * arcTanValue + (0 - joystick.stick.deltaY()) * 1.25));
+            if (joystick.data.rV <= -50) {
+                joystick.data.rV = -50
+            }
+        } else if (joystick.stick.deltaX() < -40) {
+            console.log('turning left');
+            joystick.data.lV = Math.round(-1 * (speedVector * arcTanValue) + (0 - joystick.stick.deltaY()) * 1.25);
+            if (joystick.data.lV <= -50) {
+                joystick.data.lV = -50
+            };
+            joystick.data.rV = turningSpeedBack;
+        } else if (joystick.stick.deltaX() > -25 && joystick.stick.deltaX() < 25) {
+            joystick.data.lV = -10;
+            joystick.data.rV = -10;
+        } else {
+            joystick.data.lV = 0;
+            joystick.data.rV = 0;
+        }
+    }
+
+    joystick.data.type = 'DR';
+    WebRTCDataMethold.sendData(joystick.data);
 }
 
 joystick.sendBrake = function () {
-  joystick.data = {
+    joystick.data = {
 
-    type: 'DR',
-    lV: 0,
-    rV: 0
+        type: 'DR',
+        lV: 0,
+        rV: 0
 
-  }
+    }
 
-  WebRTCDataMethold.sendData(joystick.data)
+    WebRTCDataMethold.sendData(joystick.data);
 }
